1334a0fdad60e94d3132bd45ad37b062f184d087
deepthithota5057 <thota.deepthi50@gmail.com>
2025-08-14 21:32:47 +0530

    Update zenasathleisure.py

--- zenasathleisure.py
+++ zenasathleisure.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -8,7 +8,11 @@ st.title(f"Zena's Amazing Athleisure catalog")
 
 
 # Get the current credentials
-session = get_active_session()
+#session = get_active_session()
+
+#session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('COLOR_OR_STYLE'))
 
